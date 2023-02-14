767a24890534af45fbd49b00ba526d1ff6e2955a
juanan150 <juanan0309@gmail.com>
2023-02-14 09:47:12 -0500

    add product query and mutation

--- src/graphql/product.resolvers.js
+++ src/graphql/product.resolvers.js
@@ -1,19 +1,24 @@
-const getProduct = (_, { id }) => {
-  return {
-    id: '1212',
-    name: "Prod 1",
-    price: 13.23,
-    description: "Prueba",
-    image: "www.google.com",
-    createdAt: new Date().toISOString()
-  }
-}
+const ProductsService = require('./../services/product.service');
 
-const getProducts = () => {
-  return []
-}
+const service = new ProductsService();
+
+const getProduct = async (_, { id }) => {
+  const product = await service.findOne(id);
+  return product;
+};
+
+const getProducts = async () => {
+  const products = service.find({});
+  return products;
+};
+
+const addProduct = async (_, { dto }) => {
+  const newProduct = service.create(dto);
+  return newProduct;
+};
 
 module.exports = {
   getProduct,
   getProducts,
-}
+  addProduct,
+};
